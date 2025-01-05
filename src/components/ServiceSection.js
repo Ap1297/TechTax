c13a95fd6ffc4fa700c87135b4098d1299b46974
Ankit <40174133+Ap1297@users.noreply.github.com>
2025-01-05 23:52:58 +0400

    Section.js added
    Signed-off-by: Ankit <40174133+Ap1297@users.noreply.github.com>

--- src/components/ServiceSection.js
+++ src/components/ServiceSection.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import '../stylesheet/ServiceSection.css'
+import '../stylesheet/ServiceSection.css';
+import taxConsultant from "../assets/image_1.jpg";
+import softwareServiceImage from "../assets/image_2.jpg";
 
 const ServiceSection = () => {
   return (
@@ -8,23 +10,24 @@ const ServiceSection = () => {
         <div className="service-content">
           <h2>Tax Consultation Services</h2>
           <p>
-            Welcome to DKK Accounting, your trusted partner for over a decade in Dubai! We deliver tailored solutions to meet all your business needs. Our services include precise Accounting, Bookkeeping, Taxation Consultancy, CFO Services, Business Advisory, ERP & IT Solutions, Business Setup, and Auditing Assistance.
+            Welcome to TechTax Accounting, your trusted partner for over a decade in Dubai! We deliver tailored solutions to meet all your business needs. Our services include precise Accounting, Bookkeeping, Taxation Consultancy, CFO Services, Business Advisory, ERP & IT Solutions, Business Setup, and Auditing Assistance.
           </p>
         </div>
         <div className="service-image">
-          <img src="../logo.png" alt="Tax Consultation" />
+          <img src={taxConsultant} alt="Tax Consultation" />
         </div>
       </div>
 
       <div className="service service-right">
-        <div className="service-image">
-          <img src="../hero-image.jpg" alt="IT Software Services" />
-        </div>
+        
         <div className="service-content">
           <h2>IT Software Services</h2>
           <p>
             Our IT software services empower businesses with innovative solutions, enabling seamless operations and improved productivity. From ERP systems to tailored software applications, we provide cutting-edge technology to meet your evolving business requirements.
           </p>
+        </div>
+        <div className="service-image">
+          <img src={softwareServiceImage} alt="IT Software Services" />
         </div>
       </div>
     </div>
