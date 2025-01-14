969b8696fac747906b8a9cd9e8cca8e48b117645
Ankit Panchal <ankitp@voizzit.com>
2025-01-14 11:37:37 +0400

    COrs error issues

--- src/serverComponents/server.js
+++ src/serverComponents/server.js
@@ -4,6 +4,8 @@ const PORT = process.env.PORT || 5000;
 const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
 const rtsIndex = require('../serverComponents/routes/index.router');
+const cors = require("cors");
+
 mongoose.connect("mongodb+srv://dbUser:dbUser@cluster0.3flln.mongodb.net/TechTax?retryWrites=true&w=majority&appName=Cluster0", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
